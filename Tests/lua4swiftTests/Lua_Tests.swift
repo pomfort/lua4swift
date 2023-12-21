e7a2d65411f29e3f7a3b414e7165ca4cb96dad5f
Konstantin Silbermann <kos@pomfort.com>
2023-12-21 12:48:27 +0100

    fix tests

--- Tests/lua4swiftTests/Lua_Tests.swift
+++ Tests/lua4swiftTests/Lua_Tests.swift
@@ -4,106 +4,95 @@ import XCTest
 @testable import lua4swift
 
 class Lua_Tests: XCTestCase {
-
-  func testFundamentals() {
-    let vm = VirtualMachine()
-    let table = vm.createTable()
-    table[3] = "foo"
-    XCTAssert(table[3] is String)
-    XCTAssertEqual(table[3] as! String, "foo")
-  }
-
-  func testStringX() {
-    let vm = VirtualMachine()
-
-    let stringxLib = vm.createTable()
-
-    stringxLib["split"] = vm.createFunction([String.arg, String.arg]) { args in
-      let (subject, separator) = (args.string, args.string)
-      let fragments = subject.components(separatedBy: separator)
-
-      let results = vm.createTable()
-      for (i, fragment) in fragments.enumerated() {
-        results[i+1] = fragment
-      }
-      return .value(results)
+    func testFundamentals() {
+        let vm = Lua.VirtualMachine()
+        let table = vm.createTable()
+        table[3] = "foo"
+        XCTAssert(table[3] is String)
+        XCTAssertEqual(table[3] as! String, "foo")
     }
 
-    vm.globals["stringx"] = stringxLib
+    func testStringX() {
+        let vm = Lua.VirtualMachine()
 
-    switch vm.eval("return stringx.split('hello world', ' ')", args: []) {
-      case let .values(values):
-        XCTAssertEqual(values.count, 1)
-        XCTAssert(values[0] is Table)
-        let array: [String] = (values[0] as! Table).asSequence()
-        XCTAssertEqual(array, ["hello", "world"])
-      case let .error(e):
-        XCTFail(e)
-    }
-  }
+        let stringxLib = vm.createTable()
 
-  func testCustomType() {
+        stringxLib["split"] = vm.createFunction { (subject: String, separator: String) in
+            let fragments = subject.components(separatedBy: separator)
 
-    class Note : CustomTypeInstance {
-      var name = ""
-      static func luaTypeName() -> String {
-        return "note"
-      }
-    }
-    
-    let vm = VirtualMachine()
+            let results = vm.createTable()
+            for (i, fragment) in fragments.enumerated() {
+                results[i+1] = fragment
+            }
+            return results
+        }
 
-    let noteLib:CustomType<Note> = vm.createCustomType {
-      type in
-      type["setName"] = type.createMethod([String.arg]) {
-        note, args in
-        note.name = args.string
-        return .nothing
-      }
-      type["getName"] = type.createMethod([]) {
-        note, args in
-        return .value(note.name)
-      }
+        vm.globals["stringx"] = stringxLib
+
+        do {
+            let values = try vm.eval("return stringx.split('hello world', ' ')")
+            XCTAssertEqual(values.count, 1)
+            XCTAssert(values[0] is Lua.Table)
+            let array: [String] = (values[0] as! Lua.Table).asSequence()
+            XCTAssertEqual(array, ["hello", "world"])
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
-    noteLib["new"] = vm.createFunction([String.arg]) {
-      args in
-      let note = Note()
-      note.name = args.string
-      let data = vm.createUserdata(note)
-      return .value(data)
+    func testCustomType() {
+        class Note: LuaCustomTypeInstance {
+            var name = ""
+            static func luaTypeName() -> String {
+                return "note"
+            }
+        }
+
+        let vm = Lua.VirtualMachine()
+
+        let noteLib: Lua.CustomType<Note> = vm.createCustomType { type in
+            type["setName"] = type.createMethod { (self: Note, name: String) in
+                self.name = name
+            }
+            type["getName"] = type.createMethod { (self: Note) in
+                self.name
+            }
+        }
+
+        noteLib["new"] = vm.createFunction { (name: String) in
+            let note = Note()
+            note.name = name
+            let data = vm.createUserdata(note)
+            return data
+        }
+
+        // setup the note class
+        vm.globals["note"] = noteLib
+
+        _ = try! vm.eval("myNote = note.new('a custom note')")
+        XCTAssert(vm.globals["myNote"] is Lua.Userdata)
+
+        // extract the note
+        // and see if the name is the same
+
+        let myNote: Note = (vm.globals["myNote"] as! Lua.Userdata).toCustomType()
+        XCTAssert(myNote.name == "a custom note")
+
+        // This is just to highlight changes in Swift
+        // will get reflected in Lua as well
+        // TODO: redirect output from Lua to check if both
+        // are equal
+
+        myNote.name = "now from XCTest"
+        _ = try! vm.eval("print(myNote:getName())")
+
+        // further checks to change name in Lua
+        // and see change reflected in the Swift object
+
+        _ = try! vm.eval("myNote:setName('even')")
+        XCTAssert(myNote.name == "even")
+
+        _ = try! vm.eval("myNote:setName('odd')")
+        XCTAssert(myNote.name == "odd")
     }
-
-    // setup the note class
-    vm.globals["note"] = noteLib
-
-    _ = vm.eval("myNote = note.new('a custom note')")
-    XCTAssert(vm.globals["myNote"] is Userdata)
-
-    // extract the note
-    // and see if the name is the same
-
-    let myNote:Note = (vm.globals["myNote"] as! Userdata).toCustomType()
-    XCTAssert(myNote.name == "a custom note")
-
-    // This is just to highlight changes in Swift
-    // will get reflected in Lua as well
-    // TODO: redirect output from Lua to check if both
-    // are equal
-
-    myNote.name = "now from XCTest"
-    _ = vm.eval("print(myNote:getName())")
-
-    // further checks to change name in Lua
-    // and see change reflected in the Swift object
-
-    _ = vm.eval("myNote:setName('even')")
-    XCTAssert(myNote.name == "even")
-
-    _ = vm.eval("myNote:setName('odd')")
-    XCTAssert(myNote.name == "odd")
-
-  }
-
-
 }
