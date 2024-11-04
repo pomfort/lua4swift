deff974839a79e7112eda1e5eb268f93e3d86dde
Konstantin Silbermann <kos@pomfort.com>
2024-11-04 14:52:28 +0100

    re-add the noreturn

--- Sources/CLua/include/lua.h
+++ Sources/CLua/include/lua.h
@@ -346,6 +346,7 @@ LUA_API int (lua_gc) (lua_State *L, int what, ...);
 ** miscellaneous functions
 */
 
+__attribute__((noreturn))
 LUA_API int   (lua_error) (lua_State *L);
 
 LUA_API int   (lua_next) (lua_State *L, int idx);
